f277dbff013219bb7136a9be0302e60540779455
Marcelo Sica <marcelosica10@gmail.com>
2023-03-20 09:00:42 +0000

    Proyecto Traffic Light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/index.css"
 
 //include images into your bundle
 
 //create your first component
 const Click = () => {
-	const[color, setColor] = useState("")
+  const [color, setColor] = useState("")
 
-	useEffect(()=>{console.log(color)},[color])
+  useEffect(() => { console.log(color) }, [color])
 
   return (<>
-      <h1 className="">Traffic Light</h1>
-     
-          <div className={"rounded-circle " + `red-light` + `${color==="rounded-circle red-light"? " red-light-active" : ""}`} onClick={(e)=>{setColor(e.target.className)}}></div> 
-		  <br/>
-          <div className={"rounded-circle " + `yellow-light` + `${color==="rounded-circle yellow-light"? " yellow-light-active" : ""}`} onClick={(e)=>{setColor(e.target.className)}}></div> 
-		  <br/>
-          <div className={"rounded-circle " + `green-light` + `${color==="rounded-circle green-light"? " green-light-active" : ""}`} onClick={(e)=>{setColor(e.target.className)}}></div>
-        
-      </>
-    
+    <h1>Traffic Light</h1>
+    <div className="ligths">
+      <div className={"rounded-circle " + `red-light` + `${color === "rounded-circle red-light" ? " red-light-active" : ""}`} onClick={(e) => { setColor(e.target.className) }}></div>
+      <br />
+      <div className={"rounded-circle " + `yellow-light` + `${color === "rounded-circle yellow-light" ? " yellow-light-active" : ""}`} onClick={(e) => { setColor(e.target.className) }}></div>
+      <br />
+      <div className={"rounded-circle " + `green-light` + `${color === "rounded-circle green-light" ? " green-light-active" : ""}`} onClick={(e) => { setColor(e.target.className) }}></div>
+    </div>
+  </>
+
   );
 };
 
